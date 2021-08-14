4ce5bcc5a046bf6ae9c5a80c537630460ea3e09a
Tunghsiao Liu <t@sparanoid.com>
2021-08-14 13:29:06 -0400

    feat: support douyin live

--- packages/extractor-douyin/src/index.js
+++ packages/extractor-douyin/src/index.js
@@ -4,10 +4,21 @@ import jsdom from 'jsdom';
 const { JSDOM } = jsdom;
 
 async function extract(url, options = {}) {
+  const parsedUrl = new URL(url);
+
   try {
     const resp = await got(url, {
       headers: {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+        // Douyin videos need desktop UA to work:
+        // macOS: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+        //
+        // Douyin Live streams need mobile UA to work:
+        // Android: 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36'
+        // Telegram In-App Browser: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+        'user-agent': parsedUrl.hostname === 'live.douyin.com' || parsedUrl.hostname === 'webcast.amemv.com' ?
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1' :
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+        ,
       },
       ...options
     });
--- packages/extractor-douyin/src/index.test.js
+++ packages/extractor-douyin/src/index.test.js
@@ -2,12 +2,17 @@ import { equal } from 'assert/strict';
 import extract from './index.js';
 
 const resp = await extract(`https://www.douyin.com/user/MS4wLjABAAAA5ZrIrbgva_HMeHuNn64goOD2XYnk4ItSypgRHlbSh1c`);
-const liveResp = await extract(`https://webcast.amemv.com/webcast/reflow/6988103996141488910`);
+const liveDesktopResp = await extract(`https://live.douyin.com/820648166099`);
+const liveMobileResp = await extract(`https://webcast.amemv.com/webcast/reflow/6996256987986021157`);
 
 export function jsonContentExists() {
   equal(resp?._location, '/user/MS4wLjABAAAA5ZrIrbgva_HMeHuNn64goOD2XYnk4ItSypgRHlbSh1c');
 };
 
-export function jsonLiveContentExists() {
-  equal(liveResp?.['/webcast/reflow/:id']?.room?.owner?.short_id, 3819974253);
+export function jsonLiveDesktopContentExists() {
+  equal(liveDesktopResp?.location, '/820648166099');
+};
+
+export function jsonLiveMobileContentExists() {
+  equal(liveMobileResp?.['/webcast/reflow/:id']?.room?.owner?.web_rid, '820648166099');
 };
